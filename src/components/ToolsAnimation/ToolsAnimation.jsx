3e2e3db4b156d125330c4b9d8e2142aec62be81d
Guilherme Viana <guilhermehviana01@gmail.com>
2022-12-07 23:57:51 -0300

    style update

--- src/components/ToolsAnimation/ToolsAnimation.jsx
+++ src/components/ToolsAnimation/ToolsAnimation.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import reactLogo from '../../images/react.svg';
 
-import chartJsLogo from '../../images/chartjs.png';
+import reactGoogleChartLogo from '../../images/react-google-chart.png';
 import './index.css';
 
 export default function ToolsAnimation() {
   return (
-    <div className="tools-animation">
-      <img
-        src={reactLogo}
-        alt="logo"
-        height="120px"
-        className="react-logo"
-      />
-      <p className="plus">+</p>
-      <img
-        src={chartJsLogo}
-        alt="logo"
-        height="120px"
-        className="chart-logo"
-      />
-    </div>
+    <>
+      <div className="tools-animation">
+        <img
+          src={reactLogo}
+          alt="reactjs"
+          height="120px"
+          className="react-logo"
+        />
+        <p className="plus">+</p>
+        <img
+          src={reactGoogleChartLogo}
+          alt="react google charts"
+          height="120px"
+          className="chart-logo"
+        />
+      </div>
+      <div className="techs-box">
+        <p>ReactJS</p>
+        <p>React Google Charts</p>
+      </div>
+    </>
   );
 }
