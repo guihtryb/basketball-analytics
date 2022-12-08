27c46d755b2d7aabf1a6f3e6f7bf44f83313432b
Guilherme Viana <guilhermehviana01@gmail.com>
2022-12-07 23:04:20 -0300

    feat: add react google charts to project

--- src/components/DuoGraphicList/DuoGraphicList.jsx
+++ src/components/DuoGraphicList/DuoGraphicList.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Chart from 'react-google-charts';
 
 export default function DuoGraphicList({
   graphicName,
@@ -8,17 +9,27 @@ export default function DuoGraphicList({
   rightName,
   rightData,
 }) {
+  const data = [
+    [graphicName, graphicName],
+    [leftName, leftData],
+    [rightName, rightData],
+  ];
+
+  const options = {
+    title: graphicName,
+    pieHole: 0.4,
+    is3D: false,
+  };
+
   return (
     <div className="graphic">
-      <h3>{graphicName}</h3>
-      <ul>
-        <li>
-          {`${leftName}: ${leftData}`}
-        </li>
-        <li>
-          {`${rightName}: ${rightData}`}
-        </li>
-      </ul>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width="100%"
+        height="200px"
+      />
     </div>
   );
 }
